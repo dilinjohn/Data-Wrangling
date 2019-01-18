72b4b3a89165a0382bcbfc1b166ac23a2c44e59c
dilinjohn <dilinkaithav@gmail.com>
2019-01-18 09:54:15 +0800

    Problem set 2 airport and carrier python files

---
+++ Problem Set 2 - 01 - Extract carrier list.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_carriers(page):
+    data = []
+
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        for carrier in soup.find(id="CarrierList").find_all('option'):
+        	if len(carrier['value']) ==2:
+        		data.append(carrier['value'])
+    return data
+
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+    airport = data["airport"]
+    carrier = data["carrier"]
+
+    r = s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+               data = (("__EVENTTARGET", ""),
+                       ("__EVENTARGUMENT", ""),
+                       ("__VIEWSTATE", viewstate),
+                       ("__VIEWSTATEGENERATOR",viewstategenerator),
+                       ("__EVENTVALIDATION", eventvalidation),
+                       ("CarrierList", carrier),
+                       ("AirportList", airport),
+                       ("Submit", "Submit")))
+
+    return r.text
+
+
+def test():
+    data = extract_carriers(html_page)
+    assert len(data) == 16
+    assert "FL" in data
+    assert "NK" in data
+
+if __name__ == "__main__":
+    test()
---
+++ Problem Set 2 - 02 - Extract airport list.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+html_page = "options.html"
+
+
+def extract_airports(page):
+    data = []
+
+    with open(page, "r") as html:
+        # do something here to find the necessary values
+        soup = BeautifulSoup(html, "lxml")
+        for airport in soup.find(id="AirportList").find_all('option'):
+            if len(airport['value']) == 3:
+                if airport['value'] != "All":
+                    data.append(airport['value'])
+    return data
+
+def make_request(data):
+    eventvalidation = data["eventvalidation"]
+    viewstate = data["viewstate"]
+    airport = data["airport"]
+    carrier = data["carrier"]
+
+    r = s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+               data = (("__EVENTTARGET", ""),
+                       ("__EVENTARGUMENT", ""),
+                       ("__VIEWSTATE", viewstate),
+                       ("__VIEWSTATEGENERATOR",viewstategenerator),
+                       ("__EVENTVALIDATION", eventvalidation),
+                       ("CarrierList", carrier),
+                       ("AirportList", airport),
+                       ("Submit", "Submit")))
+
+    return r.text
+
+
+def test():
+    data = extract_airports(html_page)
+    assert len(data) == 15
+    assert "ATL" in data
+    assert "ABR" in data
+
+if __name__ == "__main__":
+    test()
